842662271179426e686d542f3c7fd83ab026b9c3
Churchill Lee <churchill.lee@zenu.com.au>
2023-08-28 10:27:18 +1000

    feat: upgrade to 5.9.1

--- flake.nix
+++ flake.nix
@@ -10,11 +10,12 @@
       {
         formatter = pkgs.nixpkgs-fmt;
         packages.default = pkgs.stdenv.mkDerivation {
+          pname = "commandbox";
+          version = "5.9.1";
           src = pkgs.fetchurl {
-            url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.9.0/commandbox-bin-5.9.0.zip";
-            sha256 = "sha256-v/arhJvaMj2BgvY/v4n8GHNjPBW5l0TySPb+CtQBGi4=";
+            url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.9.1/commandbox-bin-5.9.1.zip";
+            sha256 = "d0f395dc8a27ff26528a5e6562d2c1b90770380387fe00cce9d3d1387e0ac466";
           };
-          name = "commandbox-5.9.0";
           inherit system;
           dontUnpack = true;
           meta.mainProgram = "box";
