c812a0ec53313d0f80117b23febc66742a605557
Churchill Lee <hello@feliciousx.dev>
2023-09-17 23:21:07 +1000

    feat: separate commandbox derivation

---
+++ commandbox.nix
@@ -0,0 +1,16 @@
+{ stdenv, fetchzip }:
+stdenv.mkDerivation {
+  pname = "commandbox";
+  version = "5.9.1";
+  src = fetchzip{
+    url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.9.1/commandbox-bin-5.9.1.zip";
+    sha256 = "sha256-o/QMzEneKpUInbzUW1ioV0VyDLyjmwqyN9FnDhNYY2Q=";
+  };
+  meta.mainProgram = "box";
+  installPhase = ''
+    runHook preInstall
+    mkdir -p $out/bin
+    mv box $out/bin/box
+    runHook postInstall
+  '';
+}
--- flake.nix
+++ flake.nix
@@ -4,29 +4,20 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  outputs = { self, nixpkgs, flake-utils, }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let pkgs = import nixpkgs { inherit system; }; in
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
       {
-        formatter = pkgs.nixpkgs-fmt;
-        packages.default = pkgs.stdenv.mkDerivation {
-          pname = "commandbox";
-          version = "5.9.1";
-          src = pkgs.fetchurl {
-            url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.9.1/commandbox-bin-5.9.1.zip";
-            sha256 = "d0f395dc8a27ff26528a5e6562d2c1b90770380387fe00cce9d3d1387e0ac466";
-          };
-          inherit system;
-          dontUnpack = true;
-          meta.mainProgram = "box";
-          buildInputs = [ pkgs.unzip ];
-          buildPhase = ''
-            unzip $src
-          '';
-          installPhase = ''
-            mkdir -p $out/bin
-            mv box $out/bin/box
-          '';
+        packages.default = import ./commandbox.nix {
+          stdenv = pkgs.stdenv;
+          fetchzip = pkgs.fetchzip;
+        };
+
+        apps.default = {
+          type = "app";
+          program = "${self.packages.${system}.default}/bin/box";
         };
       }
     );
