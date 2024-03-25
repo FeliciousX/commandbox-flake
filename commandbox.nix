689715bb52d94379e1454b6fd6a200bf13da0478
Churchill Lee <hello@feliciousx.dev>
2024-03-26 10:51:11 +1100

    feat: upgrade to 6.0.0

--- commandbox.nix
+++ commandbox.nix
@@ -1,10 +1,10 @@
 { stdenv, fetchzip }:
 stdenv.mkDerivation {
   pname = "commandbox";
-  version = "5.9.1";
+  version = "6.0.0";
   src = fetchzip {
-    url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/5.9.1/commandbox-bin-5.9.1.zip";
-    sha256 = "o/QMzEneKpUInbzUW1ioV0VyDLyjmwqyN9FnDhNYY2Q=";
+    url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/6.0.0/commandbox-bin-6.0.0.zip";
+    sha256 = "OHRpVq3BTpGW65Zw5ZnEgtq9L4LkCmJWvI4mpENaSIs=";
   };
   meta.mainProgram = "box";
   installPhase = ''
