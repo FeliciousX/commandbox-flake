34b61d5bbce15a55ec15460186397ac1fe8310fd
FeliciousX <hello@feliciousx.dev>
2024-06-18 08:57:21 +1000

    feat: add runtime deps for commandbox

--- commandbox.nix
+++ commandbox.nix
@@ -1,12 +1,18 @@
-{ stdenv, fetchzip }:
-stdenv.mkDerivation {
+{ pkgs }:
+pkgs.stdenv.mkDerivation {
   pname = "commandbox";
   version = "6.0.0";
-  src = fetchzip {
+  src = pkgs.fetchzip {
     url = "https://downloads.ortussolutions.com/ortussolutions/commandbox/6.0.0/commandbox-bin-6.0.0.zip";
     sha256 = "4zhjtqb62U2RQZRZbFLFfL1Etykx1Rchosy6Nu+fMiU=";
   };
   meta.mainProgram = "box";
+
+  buildInputs = [
+    pkgs.which # box uses which on certain commands on runtime
+    pkgs.jdk11_headless # TODO: figure out if user can override this
+  ];
+
   installPhase = ''
     runHook preInstall
     mkdir -p $out/bin
--- flake.nix
+++ flake.nix
@@ -3,20 +3,15 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
 
-  outputs = { self, nixpkgs, flake-utils, flake-compat }:
+  outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
       in
       {
         packages.default = import ./commandbox.nix {
-          stdenv = pkgs.stdenv;
-          fetchzip = pkgs.fetchzip;
+          inherit pkgs;
         };
 
         apps.default = {
