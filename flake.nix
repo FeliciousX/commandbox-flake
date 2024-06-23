6c043105da847f9684df3935cde8778532dae75c
FeliciousX <hello@feliciousx.dev>
2024-06-23 16:51:45 +1000

    feat(inputs): re-add flake-compat for default.nix and shell.nix support

--- flake.nix
+++ flake.nix
@@ -3,8 +3,9 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
