e4d6937e2daccd7a319c21f4109c7233b2dc148d
FeliciousX <hello@feliciousx.dev>
2024-08-28 15:15:24 +1000

    feat(flake.nix): use nix-systems

--- flake.nix
+++ flake.nix
@@ -2,21 +2,25 @@
   description = "CommandBox ColdFusion (CFML) CLI, Package Manager, REPL and much more!";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.systems.url = "github:nix-systems/default";
   inputs.flake-compat.url = "https://flakehub.com/f/edolstra/flake-compat/1.tar.gz";
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        packages.default = pkgs.callPackage ./commandbox.nix { };
-
-        apps.default = {
+  outputs = { self, nixpkgs, systems, ... }:
+    let eachSystem = nixpkgs.lib.genAttrs (import systems);
+    in {
+      packages = eachSystem (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.callPackage ./commandbox.nix { };
+        }
+      );
+      apps = eachSystem (system: {
+        default = {
           type = "app";
           program = "${self.packages.${system}.default}/bin/box";
         };
-      }
-    );
+      });
+    };
 }
